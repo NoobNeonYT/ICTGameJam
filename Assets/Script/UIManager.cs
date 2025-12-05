1d2a0e4e32d17243698b666dad24f701c45c3bc8
LXirasoL <khuan101048@gmail.com>
2025-12-05 23:53:26 +0700

    2

--- Assets/Script/UIManager.cs
+++ Assets/Script/UIManager.cs
@@ -1,29 +1,75 @@
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    [Header("Canvas")]
     [SerializeField] private GameObject menuCanvas;
     [SerializeField] private GameObject inGameCanvas;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Scene")]
+    [SerializeField] private string gameSceneName = "GameScene";
+
+    [Header("Audio")]
+    [SerializeField] private AudioMixer audioMixer;
+    [SerializeField] private Slider volumeSlider;
+    [SerializeField] private string volumeParameter = "MasterVolume";
+
+    private void Start()
     {
         ShowMenu();
-    }
-    public void ShowMenu() 
-    { 
-        menuCanvas.SetActive(true); 
-        inGameCanvas.SetActive(false); 
-    }
-    public void InGame() 
-    { 
-        menuCanvas.SetActive(false); 
-        inGameCanvas.SetActive(true); 
+
+        // โหลดค่า Volume จาก Mixer → ตั้งค่า Slider
+        if (audioMixer.GetFloat(volumeParameter, out float value))
+        {
+            volumeSlider.value = Mathf.Pow(10, value / 20);  // Convert dB → linear 0–1
+        }
+
+        // ฟัง event เวลาเลื่อน Slider
+        volumeSlider.onValueChanged.AddListener(SetVolume);
     }
 
-    // Update is called once per frame
-    void Update()
+    // -------------------------
+    //     UI Page Switching
+    // -------------------------
+    public void ShowMenu()
     {
-        
+        menuCanvas.SetActive(true);
+        inGameCanvas.SetActive(false);
+    }
+
+    public void InGame()
+    {
+        menuCanvas.SetActive(false);
+        inGameCanvas.SetActive(true);
+    }
+
+    // -------------------------
+    //     START GAME
+    // -------------------------
+    public void StartGame()
+    {
+        SceneManager.LoadScene(gameSceneName);
+    }
+
+    // -------------------------
+    //     QUIT GAME
+    // -------------------------
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game!");
+        Application.Quit();
+    }
+
+    // -------------------------
+    //     VOLUME CONTROL
+    // -------------------------
+    private void SetVolume(float value)
+    {
+        // Linear 0–1 → dB
+        float dB = Mathf.Log10(value) * 20;
+        audioMixer.SetFloat(volumeParameter, dB);
     }
 }
